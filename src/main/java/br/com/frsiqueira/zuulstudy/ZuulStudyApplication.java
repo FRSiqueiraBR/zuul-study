b66731a905bad3dc836983426af6da7f29fe420c
felipe.siqueira <felipe.siqueira@b2wdigital.com>
2020-03-06 09:12:51 -0300

    Initial commit

---
+++ src/main/java/br/com/frsiqueira/zuulstudy/ZuulStudyApplication.java
@@ -0,0 +1,13 @@
+package br.com.frsiqueira.zuulstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/frsiqueira/zuulstudy/ZuulStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frsiqueira.zuulstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
