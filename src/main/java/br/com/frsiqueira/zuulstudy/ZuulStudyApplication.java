eedc3e2f8b89e18c1addf7f2ec07cee4c3ed07a1
felipe.siqueira <felipe.siqueira@b2wdigital.com>
2020-03-06 14:01:19 -0300

    Ativando zuul no serviço

--- src/main/java/br/com/frsiqueira/zuulstudy/ZuulStudyApplication.java
+++ src/main/java/br/com/frsiqueira/zuulstudy/ZuulStudyApplication.java
@@ -2,8 +2,10 @@ package br.com.frsiqueira.zuulstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulStudyApplication {
 
 	public static void main(String[] args) {
